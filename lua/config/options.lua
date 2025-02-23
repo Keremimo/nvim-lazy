8bfe7c13d3149bd5bfb96414a3b758b0077d63e3
Kerem Kilic <code@kerem.tech>
2025-02-23 17:15:21 +0100

    Add keymaps and transparency

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,4 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { "NvimTreeNormal" })
